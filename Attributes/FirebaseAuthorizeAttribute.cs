109476124aa7ac72fedb3b4919fd3ade35245b09
Nabila Choirunisa <165561784+nabilagx@users.noreply.github.com>
2025-06-16 16:33:51 +0700

    Create FirebaseAuthorizeAttribute.cs

---
+++ Attributes/FirebaseAuthorizeAttribute.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class FirebaseAuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var uidExists = context.HttpContext.Items.TryGetValue("uid", out var uidObj);
+        var uid = uidObj?.ToString();
+
+        if (!uidExists || string.IsNullOrEmpty(uid))
+        {
+            context.Result = new UnauthorizedObjectResult(new
+            {
+                status = 401,
+                message = "Token Firebase tidak valid atau tidak ditemukan"
+            });
+        }
+    }
+}
