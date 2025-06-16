e3e2ce87eee78485f769a2e15eac16cc99e324c8
Nabila Choirunisa <165561784+nabilagx@users.noreply.github.com>
2025-06-16 14:02:08 +0700

    Update PromoController.cs

--- Controllers/PromoController.cs
+++ Controllers/PromoController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace kopinang_api.Controllers
 {
-  
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PromoController : ControllerBase
